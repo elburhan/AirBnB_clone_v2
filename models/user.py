d60948dafe9e1dc6f7c44dc54df9835fada86462
elburhan <burhanbobe@icloud.com>
2024-01-18 17:11:34 +0100

    update

--- models/user.py
+++ models/user.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from models.place import Place
-from models.review import Review
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
+    """This class defines a user by various attributes"""
+
+    # Class attribute for the table name
+    __tablename__ = 'users'
+
+    # Column for email
     email = Column(String(128), nullable=False)
+
+    # Column for password
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", cascade='all, delete, delete-orphan',
-                          backref="user")
-    reviews = relationship("Review", cascade='all, delete, delete-orphan',
-                           backref="user")
+
+    # Column for first name
+    first_name = Column(String(128), nullable=True)
+
+    # Column for last name
+    last_name = Column(String(128), nullable=True)
